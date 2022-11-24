3474d9a013b3942ed76b4ec3a4c7e25488874fee
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2022-11-24 13:58:45 +0900

    create user table

---
+++ db/migrate/20221124043735_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.text :introduction
+      t.string :profile_image
+      t.boolean :admin, default: false
+
+      t.timestamps
+    end
+  end
+end
