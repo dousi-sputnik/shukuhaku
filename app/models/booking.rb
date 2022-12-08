fbc3d7c922502f6864594023e3dc2a107dc1b4f5
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2022-12-08 10:06:30 +0900

    delete add_booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,21 @@
 class Booking < ApplicationRecord
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :person, presence: true
+
+  validate :date_before_start
+  validate :start_end_check
+
+  def date_before_start
+    if start_date < Date.today
+      errors.add(:start_date, "は本日以降の日付でお願いします")
+    end
+  end
+
+  def start_end_check
+    if self.end_date < self.start_date
+      errors.add(:end_date, "は開始日以降の選択でお願いします")
+    end
+  end
 end
